c94482f659edf47d7ab5d8139dec910ee0d30c83
Anton Karavashkin <url1990@ahoo.com>
2022-04-21 10:39:27 +0400

    redacted-config-file

--- conftest.py
+++ conftest.py
@@ -7,8 +7,14 @@ from selenium import webdriver
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.get(LANDING_PAGE_URL)
-    yield driver
-    driver.quit()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--window-size=1920,1080')
+    _driver = webdriver.Chrome(options=chrome_options)
+    _driver.implicitly_wait(3)
+    _driver.get(LANDING_PAGE_URL)
+    yield _driver
+    _driver.quit()
